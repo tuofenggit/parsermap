d4d798ca51b8e4882d6845798b93ec1dfc1561c3
wangchuang <wangchuang@goldwind.com.cn>
2017-11-03 17:29:55 +0800

    init

--- src/main/java/com/wat/parsermap/controller/MapController.java
+++ src/main/java/com/wat/parsermap/controller/MapController.java
@@ -1,5 +1,8 @@
 package com.wat.parsermap.controller;
 
+import java.io.IOException;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.socket.TextMessage;
@@ -14,14 +17,17 @@ import com.wat.parsermap.util.MeshTask;
 @RestController
 public class MapController {
 
-	@RequestMapping(value="parserMap")
-	public String parserMap() {
+	@RequestMapping(value="parserMap/{id}")
+	public String parserMap(@PathVariable(value="id") String id) throws IOException {
 		String cmd = Constant.GOLDWIND_PATH + "goldWind "+Constant.MAP_PATH+"yunnanlzk-xian80-3-35.map" +" 819 " + "false" ;
 		
 		
 		//GoldwindTransferLogParser logParser = (GoldwindTransferLogParser) ApplicationContext.getBean(GoldwindTransferLogParser.class);
 		GoldwindTransferLogParser parser = new GoldwindTransferLogParser();
-		parser.setFilename("1");
+		
+		MyWebSocket.sendInfo("1");
+		
+		parser.setFilename(id);
 		ExecuteResult r = ExecuteUtil.exec(cmd, parser);
 		//MeshTask meshtask = new MeshTask("showimage");
 		//meshtask.setPid(1);
